e30e9c7bfe1a1e627f30b6c463768a0f270b9164
unknown <radowan.me@gmail.com>
2022-04-17 23:57:19 +0600

    about

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import img from '../../images/me.png'
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
+        <div className='about container d-flex flex-column justify-content-center align-items-center'>
+            <img className='rounded-circle ' src={img} alt="" />
+            <p>Ridwan Ahmead</p>
+            <p>I will be a junior font end developer next three 2-5 months.</p>
         </div>
     );
 };
