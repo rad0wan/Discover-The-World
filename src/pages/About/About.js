8de51de3e87ceba5ede4d2ae8a061d1f48956256
unknown <radowan.me@gmail.com>
2022-04-18 19:22:13 +0600

    about

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -7,7 +7,11 @@ const About = () => {
         <div className='about container d-flex flex-column justify-content-center align-items-center'>
             <img className='rounded-circle ' src={img} alt="" />
             <p>Ridwan Ahmead</p>
-            <p>I will be a junior font end developer next three 2-5 months.</p>
+            <p className='text-center'>Insallah,I will be a junior font end developer next three 2-5 months.
+                <br />I think that my biggest attribute to any success that I have had is hard work.
+                <br /> There really is no substitute for working hard.
+                <br /> so, I always work hard with brainstorming to reach my goal.
+            </p>
         </div>
     );
 };
